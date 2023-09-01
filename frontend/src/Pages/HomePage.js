fb1734a9e30b12b1b35215a85bc4738a1af3f11b
Somesh Dwivedi <116994984+Somesh0001@users.noreply.github.com>
2023-09-01 20:50:32 +0530

    Landing Page of the Urja website created

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,5 +1,17 @@
 import React from "react";
-
+import Navbar from "../components/Navbar";
+import TitleDiv from "../components/TitleDiv";
+import Home_UpcomMatch from "../components/Home_UpcomMatch";
+import Sports from "../components/Sports";
+import Footer from "../components/Footer";
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Navbar />
+      <TitleDiv />
+      <Home_UpcomMatch />
+      <Sports />
+      <Footer />
+    </div>
+  );
 };
