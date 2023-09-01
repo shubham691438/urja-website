0d36eb51553674915a8ff60d13c91d913b4bee39
Arvindh Iyer <2021ugcs013@nitjsr.ac.in>
2023-09-01 22:30:27 +0530

    fetched backend data for points

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,13 @@ const port = 5000;
 const db_connect = require("./db_connect");
 const cors = require("cors");
 require("dotenv").config();
+const pointsTable = require("./PointsTable");
 
 app.use(cors());
 app.use(express.json());
 db_connect();
+
+app.get("/points", pointsTable);
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
--- frontend/src/Pages/PointsTable.js
+++ frontend/src/Pages/PointsTable.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const PointsTable = () => {
-  return <div>points table</div>;
+  const [data, setData] = useState("");
+  async function getData() {
+    console.log(process.env.REACT_APP_BACKEND_URL);
+    let d = await fetch(`http://localhost:5000/points`, {
+      method: "get",
+    });
+    d = await d.json();
+    console.log(d.data);
+    setData(d.data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <h1>Points Table</h1>;
 };
