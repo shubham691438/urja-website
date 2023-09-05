c1e07c65aa83f4e219d6bd30cca9e8043e7c0732
shubham <ss691438@gmail.com>
2023-09-05 11:52:20 +0530

    sports card

--- frontend/src/components/SportsGrid.js
+++ frontend/src/components/SportsGrid.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../assets/styles/sportsGridStyle.css'
+import FootballImage from '../assets/images/football.png'
 
 const SportsGrid = () => {
   return (
@@ -13,34 +14,30 @@ const SportsGrid = () => {
 
         <div className="row">
           <div className="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-            <div className="icon-box">
-              <div className="icon"><i class="fas fa-futbol"></i></div>
-              <h4><a href="">Lorem Ipsum</a></h4>
-              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+            <div className="icon-box w-100 px-5">
+              <div className="icon"><img src={FootballImage}></img></div>
+              <h4 >Football</h4>
             </div>
           </div>
 
           <div className="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
-            <div className="icon-box">
-              <div className="icon"><i className="bx bx-file"></i></div>
-              <h4><a href="">Sed ut perspici</a></h4>
-              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+          <div className="icon-box w-100">
+              <div className="icon"><img src={FootballImage}></img></div>
+              <h4 >Football</h4>
             </div>
           </div>
 
           <div className="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
-            <div className="icon-box">
-              <div className="icon"><i className="bx bx-tachometer"></i></div>
-              <h4><a href="">Magni Dolores</a></h4>
-              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+            <div className="icon-box w-100">
+              <div className="icon"><img src={FootballImage}></img></div>
+              <h4 >Football</h4>
             </div>
           </div>
 
           <div className="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
-            <div className="icon-box">
-              <div className="icon"><i className="bx bx-layer"></i></div>
-              <h4><a href="">Nemo Enim</a></h4>
-              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+            <div className="icon-box w-100">
+              <div className="icon"><img src={FootballImage}></img></div>
+              <h4 >Football</h4>
             </div>
           </div>
 
