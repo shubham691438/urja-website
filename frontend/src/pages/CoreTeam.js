9265bea7bc80edf9889a7e5feb8537dea054a369
PraveenPandey987 <130021609+PraveenPandey987@users.noreply.github.com>
2023-09-07 03:22:20 +0530

    done

--- frontend/src/pages/CoreTeam.js
+++ frontend/src/pages/CoreTeam.js
@@ -1,97 +1,176 @@
-import React from "react";
-import Cards from "../components/Cards";
-// import backgroundImage from "../assets/images/bg.jpg";
-import _1 from "../assets/images/1.jpeg";
-import _2 from "../assets/images/2.jpg";
-import _3 from "../assets/images/3.jpeg";
-import _4 from "../assets/images/4.jpg";
-import _5 from "../assets/images/5.jpg";
-import _6 from "../assets/images/6.jpg";
-import _7 from "../assets/images/7.jpg";
-import _8 from "../assets/images/8.jpeg";
-import _9 from "../assets/images/9.jpeg";
-import _10 from "../assets/images/10.jpeg";
-import _11 from "../assets/images/11.jpeg";
-import _12 from "../assets/images/12.jpeg";
-import _13 from "../assets/images/13.jpeg";
-import _14 from "../assets/images/14.jpeg";
-import _15 from "../assets/images/15.jpeg";
-import _16 from "../assets/images/16.jpeg";
-import _17 from "../assets/images/17.jpeg";
-import _18 from "../assets/images/18.jpeg";
-// import logo from "../assets/images/logo.png";
+import React from 'react'
+import Cards from '../components/Cards';
+import backgroundImage from "../assets/images/bg.jpg";
+import amol from "../assets/images/Amol_sourav.jpeg"
+import prashant from "../assets/images/prashant.jpg"
+import abhinav from "../assets/images/abhinav.jpeg"
+import anubhav from "../assets/images/anubhavImg.jpg"
+import nitin from "../assets/images/nitin.jpg"
+import aman from "../assets/images/aman.jpg"
+import sudhanshu from "../assets/images/sudhanshu.jpg"
+import satish from "../assets/images/satish.jpeg"
+import shravya from "../assets/images/shravya.jpeg"
+import ravindra from "../assets/images/ravindraRajImg.jpeg"
+import nikita from "../assets/images/nikita.jpeg"
+import biswajit from "../assets/images/biswajit.jpeg"
+import kuldeep from "../assets/images/kuldeep.jpeg"
+import kumar from "../assets/images/kumar.jpeg"
+import navya from "../assets/images/navya.jpeg"
+import avantika from "../assets/images/avantika.jpeg"
+import shubham from "../assets/images/shubham.jpeg"
+import anurag from "../assets/images/anurag.jpeg"
+import logo from "../assets/images/logo.png";
 import AnimatedHeading from "../components/AnimatedHeading";
-
 function CoreTeam() {
+
   return (
-    <div>
-      <section id="team" class="team">
-        <div>
-          <div
-            class="row gy-5 "
-            style={{
-              margin: "auto",
-              display: "flex",
-              flexWrap: "wrap",
-              justifyContent: "space-evenly",
-              alignContent: "center",
-              backgroundColor: "rgb(34, 40, 49)",
-              paddingTop: "100px",
-            }}
-          >
+
+    <div >
+
+      <section id="team" class="team" style={{ backgroundColor: "rgb(34, 40, 49)", }} >
+        <div >
+          <div class="row gy-5 container " style={{
+            margin: "auto",
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-around',
+            alignContent: 'center',
+            backgroundColor: "rgb(34, 40, 49)",
+            paddingTop: "100px"
+
+          }}>
             <AnimatedHeading heading="OUR-TEAM" />
-            <Cards img={_1} Name="Amol Sourav" description="App Head" />
+            <div style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'space-around'
+            }}>
+              <Cards
+                img={ravindra}
+                Name="Ravindra Raj"
+                description="sports secretary" />
+              <Cards
+                img={anubhav}
+                Name="Anubhav"
+                description="joint sports secretary" />
+            </div>
+            <Cards
+              img={kuldeep}
+              Name="kuldeep kumar"
+              description="General secretary"
+            />
 
-            <Cards img={_2} Name="Prashant Raj" description="Event Head" />
-
-            <Cards img={_3} Name="Abhinav Kumar" description="PR Head" />
-            <Cards
-              img={_4}
-              Name="Anubhav"
-              description="Joint Sports Secretary"
-            />
-            <Cards img={_5} Name="nitin kumar" description="Joint Secretary" />
-            <Cards img={_6} Name="Aman Kumar" description="PR Head" />
-            <Cards
-              img={_7}
-              Name="Sudhanshu Bharti"
-              description="Executive Head"
-            />
-
-            <Cards img={_8} Name="Satish" description="Logistic Head" />
-
-            <Cards img={_9} Name="Shravya" description="Event Head" />
 
             <Cards
-              img={_10}
-              Name="Ravindra Raj"
-              description="Sports Secretary"
+              img={nitin}
+              Name="nitin kumar"
+              description="joint secretary"
             />
-            <Cards img={_11} Name="Nikita Sundi" description="Logistic Head" />
-            <Cards img={_12} Name="Biswajit Murmu" description="Medical Head" />
-            <Cards img={_13} Name="Kumar Abhinav" description="Medical Head" />
+
             <Cards
-              img={_14}
-              Name="Kuldeep Kumar "
-              description="General Secretary"
+              img={sudhanshu}
+              Name="Sudhanshu bharti"
+              description="executive head"
             />
             <Cards
-              img={_15}
-              Name="Navya Shankar"
-              description="Executive Head"
+              img={navya}
+              Name="Navya shankar"
+              description="Executive head" />
+
+            <Cards
+              img={prashant}
+              Name="prashant raj"
+              description="event head" />
+
+            <Cards
+              img={shravya}
+              Name="Shravya"
+              description="event head" />
+
+            <Cards
+              img={satish}
+              Name="Satish"
+              description="logistic head" />
+
+
+
+
+
+            <Cards
+              img={nikita}
+              Name="Nikita sundi"
+              description="logistic head" />
+
+            <Cards
+              img={biswajit}
+              Name="Biswajit Murmu"
+              description="medical head"
             />
             <Cards
-              img={_16}
-              Name="Avantika Tiwari"
-              description="Creative Head"
+              img={abhinav}
+              Name="Kumar abhinav"
+              description="medical head"
             />
-            <Cards img={_17} Name="Shubham Tiwary" description="App Head" />
-            <Cards img={_18} Name="Anurag Mahanty" description="CA Head" />
+
+            <Cards
+              img={amol}
+              Name="Amol sourav"
+              description="App head" />
+
+
+            <Cards
+              img={shubham}
+              Name="Shubham tiwary"
+              description="App head"
+            />
+            <Cards
+              img={avantika}
+              Name="avantika tiwari"
+              description="creative head"
+            />
+
+            <Cards
+              img={abhinav}
+              Name="Abhinav Kumar"
+              description="pr head" />
+
+
+            <Cards
+              img={aman}
+              Name="aman kumar"
+              description="pr head"
+            />
+
+
+
+
+
+
+
+
+
+            <Cards
+              img={anurag}
+              Name="Anurag Mahanty"
+              description="CA head"
+            />
+
+
+
+
+
+
+
+
           </div>
         </div>
+
+
       </section>
+
+
     </div>
-  );
+  )
 }
 
-export default CoreTeam;
+export default CoreTeam
