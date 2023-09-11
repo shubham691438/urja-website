952bc9907091410da0339ca18d4b6fbe169085be
shubham <ss691438@gmail.com>
2023-09-12 02:31:29 +0530

    changed style of result card

--- frontend/src/components/ResultCard.jsx
+++ frontend/src/components/ResultCard.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 // import { Zoom } from "react-reveal";
-import logo1 from "../assets/images/logo_1.png";
-import logo2 from "../assets/images/logo_2.png";
+
+import pieEcmImage from '../assets/images/branchImg/player1.png';
+import eceImage from '../assets/images/branchImg/player2.png';
+import cseImage from '../assets/images/branchImg/player3.png';
+import eeeImage from '../assets/images/branchImg/player4.png';
+import pgImage from '../assets/images/branchImg/player5.png';
+import meImage from '../assets/images/branchImg/player6.png';
+import ceImage from '../assets/images/branchImg/player7.png';
+import other1Image from '../assets/images/branchImg/player8.png';
+import other2Image from '../assets/images/branchImg/player9.png';
+
 
 
 const ResultCard = ({sport,gender,team1Name,team2Name,matchTitle,score,result}) => {
-  //csxsxz
+
+  const imageMap = new Map([
+    ['PIE+ECM', pieEcmImage],
+    ['ECE', eceImage],
+    ['CSE', cseImage],
+    ['EEE', eeeImage],
+    ['PG', pgImage],
+    ['ME', meImage],
+    ['CE', ceImage],
+    ['other1', other1Image],
+    ['other2', other2Image],
+  ]);
+
+  // Get the image URL based on the passed key
+  const imageUrl1 = imageMap.get(team1Name);
+  const imageUrl2 = imageMap.get(team2Name);
+
+  // If the image URL is not found (key doesn't exist), use a default image
+  const defaultImageUrl1 = other1Image;
+  const defaultImageUrl2 = other2Image;
 
   return (
     <div
@@ -14,10 +42,12 @@ const ResultCard = ({sport,gender,team1Name,team2Name,matchTitle,score,result}) 
     }}
   >
     
-    {/* <Zoom> */}
     
+    {/* <Zoom> */}
+      <div class="d-flex justify-content-center">
+      <h3><span class="badge badge-danger mb-3 ">Men Semi-final</span></h3>
+      </div>
       <div className="container " style={{paddingBottom:"50px",paddingTop:"70px"}}>
-      
         <div className="row">
         
           <div className="col-lg-12 ">
@@ -25,14 +55,12 @@ const ResultCard = ({sport,gender,team1Name,team2Name,matchTitle,score,result}) 
             <div className="d-flex team-vs result-card">
               
               <span className="score">
-                <p>{matchTitle?matchTitle:"match Title"}</p>
-                <p>{score?score:"score"}</p>
-                <p>{result?result:"result"}</p>
+                4 -1
               </span>
               <div className="team-1 w-50">
                 <div className="team-details w-100 text-center">
-                  <img src={logo1} alt="Image2" className="img-fluid" />
-                  <h3>{team1Name}</h3>
+                  <img src={imageUrl1 || defaultImageUrl1} alt="Image2" className="img-fluid" />
+                  <h3>{team1Name?team1Name:"Team A"}</h3>
                   {/* <ul className="list-unstyled">
                     <li>CSE</li>
                   </ul> */}
@@ -40,8 +68,8 @@ const ResultCard = ({sport,gender,team1Name,team2Name,matchTitle,score,result}) 
               </div>
               <div className="team-2 w-50">
                 <div className="team-details w-100 text-center">
-                  <img src={logo2} alt="Image1" className="img-fluid" />
-                  <h3>{team2Name}</h3>
+                  <img src={imageUrl2 || defaultImageUrl2} alt="Image1" className="img-fluid" />
+                  <h3>{team2Name?team2Name:"Team B"}</h3>
                   {/* <ul className="list-unstyled">
                     <li>CIVIL</li>
                   </ul> */}
