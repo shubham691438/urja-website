8a6bcf22be60263bb9a42a86a45144f0d13c439a
shubham <ss691438@gmail.com>
2023-11-03 13:12:41 +0530

    spinner

--- frontend/src/components/HomePointsTable.js
+++ frontend/src/components/HomePointsTable.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import Trophy from '@mui/icons-material/EmojiEvents';
 import { Button, Link } from "@mui/material";
+import PulseLoader from "react-spinners/PulseLoader";
 
 const HomePointsTable = () => {
-  
+
   const [points, setPoints] = useState([]);
+  const [loading,setLoading]=useState(true);
 
   useEffect(() => {
    
+    setLoading(true)
     fetch("https://script.googleusercontent.com/macros/echo?user_content_key=_x617luk9BXjMPP6lnOqnK6yeJDOru_aQIU6MWc8JfwsNe1jUN9zyZT297rgdC5Vw1iUCpBHKj0L4ztMlpOqSLiyPFo4-UFSm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnOZkPTmvpFlwOkN0oa9TzbADypue5jHObWFXiXuEr3mnHU46ALdUDYFuXhr-yE3yEza8-z-vI4QVYJ7dq-ICgOF-p6dyN7s-Rtz9Jw9Md8uu&lib=MqEGK2nOPTRqNxgnaWqkj_8HoBXLQmCkk")
       .then((response) => response.json())
       .then((data) => {
@@ -25,6 +28,7 @@ const HomePointsTable = () => {
         }));
         pointsArray.sort((a, b) => b.points - a.points);  
         setPoints(pointsArray); 
+        setLoading(false)
         // console.log(pointsArray);
       })
       .catch((error) => {
@@ -48,30 +52,45 @@ const HomePointsTable = () => {
                 <th>POINTS</th>
               </tr>
             </thead>
-            <tbody>
-              {points.map((item, index) => (
-                <tr key={index}>
-                  <td>
-                    {index === 0 && (
-                      <Trophy sx={{color:'#C9B037'}}/>
-                    )}
-                    {index === 1 && (
-                      <Trophy sx={{color:'#D7D7D7'}}/>
-                    )}
-                    {index === 2 && (
-                      <Trophy sx={{color:'#6A3805'}}/>
-                    )}
-                    {index >= 3 && index + 1}
-                  </td>
-                  <td >
-                    <strong className="text-white">
-                      {item.branch.toUpperCase()}
-                    </strong>
-                  </td>
-                  <td style={{backgroundColor:"#36454F"}}>{item.points}</td>
-                </tr>
-              ))}
-            </tbody>
+            {
+  loading ? (
+    <div className="d-flex justify-content-center">
+      <PulseLoader
+        color="#36d7b7"
+        loading={loading}
+        size={20}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
+  ) : (
+    <tbody>
+      {points.map((item, index) => (
+        <tr key={index}>
+          <td>
+            {index === 0 && (
+              <Trophy sx={{color:'#C9B037'}}/>
+            )}
+            {index === 1 && (
+              <Trophy sx={{color:'#D7D7D7'}}/>
+            )}
+            {index === 2 && (
+              <Trophy sx={{color:'#6A3805'}}/>
+            )}
+            {index >= 3 && index + 1}
+          </td>
+          <td >
+            <strong className="text-white">
+              {item.branch.toUpperCase()}
+            </strong>
+          </td>
+          <td style={{backgroundColor:"#36454F"}}>{item.points}</td>
+        </tr>
+      ))}
+    </tbody>
+  )
+}
+            
           </table>
         </div>
       </div>
