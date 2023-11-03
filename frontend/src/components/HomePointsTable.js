087a8e788e56c68a786800ecf1b9b396f5164d57
shubham <ss691438@gmail.com>
2023-11-03 12:41:27 +0530

    home points table modification

--- frontend/src/components/HomePointsTable.js
+++ frontend/src/components/HomePointsTable.js
@@ -1,23 +1,42 @@
 import React, { useState, useEffect } from "react";
 import Trophy from '@mui/icons-material/EmojiEvents';
+import { Button, Link } from "@mui/material";
 
 const HomePointsTable = () => {
   const [data, setData] = useState([]);
   const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
-  async function getData() {
-    let response = await fetch(backendUrl + "/api/medals/get-medal-table", {
-      method: "get",
-    });
-    let result = await response.json();
-    // Sort the data in decreasing order by points
-    result.medals.sort((a, b) => b.points - a.points);
-    setData(result.medals);
-  }
+  const [points, setPoints] = useState([]);
 
   useEffect(() => {
-    getData();
+   
+    fetch("https://script.googleusercontent.com/macros/echo?user_content_key=_x617luk9BXjMPP6lnOqnK6yeJDOru_aQIU6MWc8JfwsNe1jUN9zyZT297rgdC5Vw1iUCpBHKj0L4ztMlpOqSLiyPFo4-UFSm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnOZkPTmvpFlwOkN0oa9TzbADypue5jHObWFXiXuEr3mnHU46ALdUDYFuXhr-yE3yEza8-z-vI4QVYJ7dq-ICgOF-p6dyN7s-Rtz9Jw9Md8uu&lib=MqEGK2nOPTRqNxgnaWqkj_8HoBXLQmCkk")
+      .then((response) => response.json())
+      .then((data) => {
+        
+        
+        const total = data.filter((point) => point.Sport == "TOTAL");
+
+        console.log(total[0])
+        // Create an array of objects from the points object
+        const pointsArray = Object.keys(total[0])
+        .filter(key => key !== "Sport") // Exclude the "Sport" property
+        .map(branch => ({
+          "branch":branch,
+          "points": total[0][branch]
+        }));
+        pointsArray.sort((a, b) => b.points - a.points);  
+        setPoints(pointsArray); 
+        // console.log(pointsArray);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
   }, []);
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
   return (
     <div className="col-lg-12">
@@ -29,13 +48,10 @@ const HomePointsTable = () => {
                 <th>Sr. No.</th>
                 <th >Branch</th>
                 <th>POINTS</th>
-                {/* <th>Gold</th>
-                <th>Silver</th>
-                <th>Bronze</th> */}
               </tr>
             </thead>
             <tbody>
-              {data.map((item, index) => (
+              {points.map((item, index) => (
                 <tr key={index}>
                   <td>
                     {index === 0 && (
@@ -55,9 +71,6 @@ const HomePointsTable = () => {
                     </strong>
                   </td>
                   <td style={{backgroundColor:"#36454F"}}>{item.points}</td>
-                  {/* <td>{item.gold}</td>
-                  <td>{item.silver}</td>
-                  <td>{item.bronze}</td> */}
                 </tr>
               ))}
             </tbody>
